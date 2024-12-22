24f640bd9e2648f42521a6b232a6cd9899778bee
ilyavladimirov1 <ilya_vladimirov_2004@list.ru>
2024-12-23 01:23:15 +0300

    commit

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: "ЦСКА Арена",
+      link: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/CSKA_Arena_%28Quintin_Soloviev%29.jpg/1200px-CSKA_Arena_%28Quintin_Soloviev%29.jpg",
+    },
+    {
+      name: "СКА Арена",
+      link: "https://img.championat.com/s/1350x900/news/big/d/d/statistika-poseschaemosti-sezona-v-khl_17090500071008800390.jpg",
+    },
+    {
+      name: "Татнефть Арена",
+      link: "https://www.ak-bars.ru/uploads/843/album-detail/album-2821/web/b8918308af47b6c0a8b15a11904e8295.jpg",
+    },
+    {
+      name: "Уфа-Арена",
+      link: "https://api.bashinform.news/attachments/e48b7be8bd091b7d8e66505cb8a929574f2c24a9/store/crop/0/0/1280/854/1280/854/0/49e49bd222eb3f39f57df7231ad7ee6f7a1261d4eb2c8f02ee93d594b456/N4vJELw7EjM_jpg_crop1616761265.jpg",
+    },
+    {
+      name: "ВТБ Арена",
+      link: "https://s0.rbk.ru/v6_top_pics/media/img/4/26/756086363977264.jpg",
+    },
+    {
+      name: "G-Drive Арена",
+      link: "https://img.championat.com/s/1350x900/news/big/x/j/kakova-poseschaemost-khl-v-sezone-2022-2023_16699103972032991603.jpg",
+    }
+];
